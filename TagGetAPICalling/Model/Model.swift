a5246dedf1710fc00f56a9876fc91cf46d9d0682
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-10 18:57:44 -0700

    Commit

---
+++ TagGetAPICalling/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  TagGetAPICalling
+//
+//  Created by Arpit iOS Dev. on 10/06/24.
+//
+
+import Foundation
---
+++ TagGetAPICalling/View/Cell/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  TagGetAPICalling
+//
+//  Created by Arpit iOS Dev. on 10/06/24.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
